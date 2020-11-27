3052feef191c6ebf1ed371c32188d91d2a18294e
dhunt84971 <dhuntlinux@gmail.com>
2020-11-27 17:57:29 -0500

    Completed page rename and remove.

--- app_documents.js
+++ app_documents.js
@@ -6,26 +6,42 @@ var app_documents = {
     lstDocuments: {},
     dvDocuments: {},
     txtRename: {},
+    txtDoc: {},
     contextSelectedDoc: "",
+    contextSelectedPage: "",
     renameTarget: {},
     //#endregion GLOBAL DECLARATIONS
 
     //#region PAGE RENDER FUNCTIONS
     loadPages: function (docFullName) {
-        console.log(docFullName);
-        this.getPages(docFullName)
+        return new Promise((resolve, reject) => {
+            console.log(docFullName);
+            this.getPages(docFullName)
             .then((data) => {
                 emptyDiv("lstDocs");
                 console.log(data);
                 if(data){
+                    let firstPage = {};
                     for (var i = 0; i < data.length; i++) {
-                        addItemtoDiv("lstDocs", data[i].DocName, "btn srchResultItem");
+                        let pageBtnEl = addItemtoDiv("lstDocs", data[i].DocName, "btn srchResultItem", "data-grp=page");
+                        pageBtnEl.addEventListener("click", (e)=>{
+                            this.btnPage_Clicked(e.target);
+                        });
+                        pageBtnEl.addEventListener("contextmenu", (e)=>{
+                            this.btnPage_RtClicked(e);
+                        });
+                        if (i == 0) firstPage = pageBtnEl;
                     }
+                    this.selectPage(firstPage);
+                    resolve();
                 }
             })
             .catch((err) => {
                 console.log(err);
+                reject(err);
             });
+        });
+        
     },
 
     docContextMenu: function (el, fullPath) {
@@ -34,6 +50,17 @@ var app_documents = {
         var menu = document.querySelector(".docsMenu");
         menu.style.left = el.clientX + "px";
         menu.style.top = el.clientY + "px";
+        menu.classList.remove("hide");
+        console.log(menu);
+    },
+
+    pageContextMenu: function (e) {
+        let el = e.target;
+        this.contextSelectedPage = el.innerHTML;
+        console.log(e.clientX);
+        var menu = document.querySelector(".pagesMenu");
+        menu.style.left = e.clientX + "px";
+        menu.style.top = e.clientY + "px";
         menu.classList.remove("hide");
         console.log(menu);
     },
@@ -50,6 +77,22 @@ var app_documents = {
         this.txtRename.classList.remove("hide");
         this.txtRename.focus();
         this.txtRename.select();
+    },
+
+    unselectPageButtons: function () {
+        let pages = document.querySelectorAll("div[data-grp='page']");
+        for (let el of pages){
+            el.classList.remove("selected");
+        }
+    },
+
+    selectPageButton: function (el) {
+        this.unselectPageButtons();
+        el.classList.add("selected");
+    },
+
+    showPageData: function (data) {
+        this.txtDoc.value = (data) ? data : " ";
     },
 
     //#endregion PAGE RENDER FUNCTIONS
@@ -191,32 +234,17 @@ var app_documents = {
         });
     },
 
-    addPageSqlite: function (docName) {
-        return new Promise((resolve, reject) => {
-            var docFullName = parentDoc == "" ? docName : parentDoc + "/" + docName;
-            // Make sure the document name is unique.
-            this.getUniqueDocName(docFullName)
-                .then((docNewName) => {
-                    // Add the document name to the database.
-                    let db = new sqlite3.Database(dbFile, (err) => {
-                        if (err) throw err;
-                        var sql = `
-                            INSERT INTO Docs 
-                            (DocName, DocLocation, DocColor, DocText, LastModified) 
-                            VALUES ('New Page', '${docNewName}',
-                            -1, '','${getMySQLNow()}')
-                        `;
-                        console.log("Executing SQL query = " + sql);
-                        db.run(sql, (err) => {
-                            if (err) reject(err)
-                            else resolve();
-                        });
-                        db.close();
-                        this.dvDocuments.addTVItem(this.lstDocuments, docNewName, false);
-                        this.selectDocument(docNewName);
-                    });
-                });
-        });
+    addPageSqlite: function (path) {
+        // Get a unique page name.
+        let newPageName = this.getUniquePageName("New Page");
+        // Add the document to the database.
+        var sql = `
+            INSERT INTO Docs 
+            (DocName, DocLocation, DocColor, DocText, LastModified) 
+            VALUES ('${newPageName}', '${path}',
+            -1, '','${getMySQLNow()}')
+        `;
+        return this.execCommandSqlite(sql);
     },
 
     docNameExistsMySQL: function (docFullName) {
@@ -271,6 +299,30 @@ var app_documents = {
         });
     },
 
+    docPageExistsMySQL: function(fullPath, pageName){
+        // ::TODO::
+    },
+
+    docPageExistsSqlite: function(fullPath, pageName){
+        return new Promise(function (resolve, reject) {
+            let sql = `
+                SELECT DocName
+                FROM Docs 
+                WHERE DocLocation = '${fullPath}'
+                AND DocName =  '${pageName}';
+            `;
+            app_documents.execQuerySqlite(sql)
+            .then((data)=>{
+                let retValue = data.length > 0;
+                resolve(retValue);
+            })
+            .catch((err)=>{
+                console.log(err);
+                reject(err);
+            });
+        });
+    },
+
     getDocsMySQL: function () {
         return new Promise((resolve, reject)=>{
             var connection = mysql.createConnection(_settings);
@@ -315,16 +367,75 @@ var app_documents = {
         });
     },
 
-    deleteDocSqlite: function (fullPath){
+    updatePageNameSqlite: function (fullPath, oldName, newName){
         var sql = `
-            DELETE Docs 
-            WHERE INSTR(DocLocation, '${fullPath}') = 1;
+            UPDATE DOCS
+            SET DocName = '${newName}'
+            WHERE DocLocation = '${fullPath}' 
+            AND DocName = '${oldName}';
+        `;
+        return this.execCommandSqlite(sql);
+    },
+
+    deleteDocSqlite: function (fullPath){
+        var sql1 = `
+            DELETE FROM Docs 
+            WHERE DocLocation = '${fullPath}';
+        `;
+        var sql2 = `
+            DELETE FROM Docs 
+            WHERE INSTR(DocLocation, '${fullPath}/') = 1;
+        `;
+        return Promise.all([this.execCommandSqlite(sql1), this.execCommandSqlite(sql2)])
+        .catch((err)=>{
+            console.log(err);
+            ShowWarningMessageBox("Database command failure!");
+        });
+    },
+
+    deletePageMySQL: function(fullPath, pageName){
+        // ::TODO::
+    },
+
+    deletePageSqlite: function(fullPath, pageName){
+        var sql = `
+            DELETE FROM Docs 
+            WHERE DocLocation = '${fullPath}'
+            AND DocName = '${pageName}';
         `;
         return this.execCommandSqlite(sql)
         .catch((err)=>{
             console.log(err);
             ShowWarningMessageBox("Database command failure!");
         });
+    },
+
+    getPageNoteMySQL: function (fullPath, pageName){
+        // ::TODO::
+    },
+
+    getPageNoteSqlite: function (fullPath, pageName){
+        let sql = `
+            SELECT DocText FROM DOCS
+            WHERE DocLocation = '${fullPath}'
+            AND DocName = '${pageName}';
+        `;
+        return this.execQuerySqlite(sql);
+    },
+
+    updatePageMySQL: function(fullPath, pageName, docText){
+
+    },
+
+    updatePageSqlite: function(fullPath, pageName, docText){
+        let sql = `
+            UPDATE DOCS
+                SET DocText = '${sqlSafeText(docText)}',
+                LastModified = '${getMySQLNow()}' 
+            WHERE DocLocation = '${fullPath}'
+            AND DocName = '${pageName}';
+        `;
+        return this.execCommandSqlite(sql);
     },
 
     execQuerySqlite: function (sql) {
@@ -375,6 +486,12 @@ var app_documents = {
             this.getPagesSqlite(docFullName);
     },
 
+    getPageNote: function (docFullName, pageName){
+        return (_settings.dbType == "MySql") ?
+            this.getPageNoteMySQL(docFullName, pageName) :
+            this.getPageNoteSqlite(docFullName, pageName);
+    },
+
     getUniqueDocName: function (docFullName) {
         return new Promise(async (resolve, reject) => {
             var fileNameIndex = 0;
@@ -398,6 +515,12 @@ var app_documents = {
         return (_settings.dbType == "MySql") ?
             this.docNameExistsMySQL(docFullName) :
             this.docNameExistsSqlite(docFullName);
+    },
+
+    docPageExists: function (fullPath, pageName){
+        return (_settings.dbType == "MySql") ?
+            this.docPageExistsMySQL(fullPath, pageName) :
+            this.docPageExistsSqlite(fullPath, pageName);
     },
 
     getDocs: function (){
@@ -437,10 +560,32 @@ var app_documents = {
             this.addDocLocationSqlite(parentDoc, docName);
     },
 
-    addPage: function (docName) {
+    addPage: function (path) {
         return (_settings.dbType == "MySql") ?
-            this.addPageMySQL(parentDoc, docName) :
-            this.addPageSqlite(parentDoc, docName);
+            this.addPageMySQL(path) :
+            this.addPageSqlite(path);
+    },
+
+    addPage_Clicked: function(){
+        let path = this.dvDocuments.getSelectedFullPath();
+        console.log(path);
+        this.addPage(path)
+        .then(()=>{
+            this.loadPages(path);
+        });
+    },
+
+    updatePage: function(path, pageName, docText){
+        return (_settings.dbType == "MySql") ?
+            this.updatePageMySQL(path, pageName, docText) :
+            this.updatePageSqlite(path, pageName, docText);
+    },
+
+    savePage: function(){
+        let path = this.dvDocuments.getSelectedFullPath();
+        let pageName = this.getSelectedPageName();
+        let docText = this.txtDoc.value;
+        this.updatePage(path, pageName, docText);
     },
 
     getUniqueDocName: function (docFullName) {
@@ -462,13 +607,36 @@ var app_documents = {
         });
     },
 
+    getUniquePageName: function (pageName) {
+        let pages = document.querySelectorAll("div[data-grp='page']");
+        console.log(pages);
+        let newPageName = pageName;
+        let idx = 0;
+        for (let el of pages){
+            if (el.innerHTML == newPageName){
+                idx += 1;
+                newPageName = `${pageName} (${idx})`;
+            }
+        }
+        return newPageName;
+    },
+
     updateDocName: function (oldDocFullPath, newDocFullPath) {
         return this.updateDocNameSqlite(oldDocFullPath, newDocFullPath);
+    },
+
+    updatePageName: function (fullPath, oldName, newName) {
+        return this.updatePageNameSqlite(fullPath, oldName, newName);
     },
 
     deleteDoc: function (fullPath){
         return this.deleteDocSqlite(fullPath);
     },
+
+    deletePage: function (fullPath, pageName){
+        return this.deletePageSqlite(fullPath, pageName);
+    },
+
     //#endregion DATA TRANSMITTAL FUNCTIONS
 
     //#region DOCUMENT MANAGEMENT FUNCTIONS
@@ -483,17 +651,31 @@ var app_documents = {
         this.renameTarget = "document";
     },
 
+    renamePage_Clicked: function (fullPath, pageName) {
+        let el = this.getSelectedPageElement();
+        this.showtxtRename(el);
+        this.renameTarget = "page";
+    },
+
     renamed: async function (){
         let newName = this.txtRename.value;
-        let newFullPath = "";
         if (this.renameTarget == "document"){
-            newFullPath = await this.renameDoc(newName);
+            let newFullPath = await this.renameDoc(newName);
+            this.loadDocs()
+            .then (()=>{
+                this.dvDocuments.setSelectedPath(newFullPath);
+            });
+        }
+        else{
+            let fullPath = this.dvDocuments.getSelectedFullPath();
+            let oldName = this.getSelectedPageName();
+            let newPageName = await this.renamePage(fullPath, oldName, newName);
+            this.loadPages(this.dvDocuments.getSelectedFullPath())
+            .then(()=>{
+                this.selectPageByName(newPageName);
+            });
         }
         this.txtRename.classList.add("hide");
-        this.loadDocs()
-        .then (()=>{
-            this.dvDocuments.setSelectedPath(newFullPath);
-        });
     },
 
     renameDoc: function(newName){
@@ -519,6 +701,22 @@ var app_documents = {
         });
     },
 
+    renamePage: function (fullPath, oldName, newName){
+        return new Promise (async (resolve, reject) => {
+            let exists = await this.docPageExists(fullPath, newName);
+            if (exists == true){
+                ShowWarningMessageBox("Name already exists!");
+                reject("Name already exists!");
+            }
+            else{
+                await this.updatePageName(fullPath, oldName, newName)
+                .catch((err)=>{reject(err);});
+                resolve(newName);
+                return newName;
+            }
+        });
+    },
+
     removeDoc_Clicked: function (docName) {
         if (showConfirmationBox("Are you sure?\nNote: Deleting this document deletes all children.")){
             this.deleteDoc(docName)
@@ -528,16 +726,66 @@ var app_documents = {
         }
     },
 
+    removePage_Clicked: function (pageName) {
+        if (showConfirmationBox("Are you sure?")){
+            let fullPath = this.dvDocuments.getSelectedFullPath();
+            this.deletePage(fullPath, pageName)
+            .then(()=>{
+                this.loadPages(this.dvDocuments.getSelectedFullPath());
+            });
+        }
+    },
+
+    selectPage: function(el){
+        this.selectPageButton(el);
+        let fullPath = this.dvDocuments.getSelectedFullPath();
+        let pageName = el.innerHTML;
+        this.getPageNote(fullPath, pageName)
+        .then((data)=>{
+            this.showPageData(data[0].DocText);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    },
+
+    selectPageByName: function(name){
+        let pages = document.querySelectorAll("div[data-grp='page']");
+        let elSelect = {};
+        for (let el of pages){
+            if (el.innerHTML == name){
+                elSelect = el;
+            }
+        }
+        this.selectPage(elSelect);
+    },
+
+    btnPage_Clicked: function (el) {
+        this.selectPage(el);
+    },
+
+    btnPage_RtClicked: function (e) {
+        this.pageContextMenu(e);
+    },
+
     //#endregion DOCUMENT MANAGEMENT FUNCTIONS
 
     //#region HELPER FUNCTIONS
+    getSelectedPageName: function(){
+        let el = this.getSelectedPageElement();
+        return el.innerHTML;
+    },
 
+    getSelectedPageElement: function(){
+        return document.querySelector("#lstDocs .selected");
+    },
     //#endregion HELPER FUNCTIONS
 
     //#region INITIALIZATION
     init: function () {
         this.lstDocuments = document.getElementById("lstDocuments");
         this.txtRename = document.getElementById("txtRename");
+        this.txtDoc = document.getElementById("txtDoc");
         this.dvDocuments = new div_treeview(this.lstDocuments, "/");
         this.dvDocuments.onSelect((text)=> {
             this.selectDocument(text);
@@ -558,7 +806,7 @@ document.getElementById("btnAddDoc").addEventListener("click", () => {
 });
 
 document.getElementById("btnAddPage").addEventListener("click", () => {
-    app_documents.addPage("", "New Document");
+    app_documents.addPage_Clicked();
 });
 
 document.getElementById("txtRename").addEventListener("keyup", (e) =>{
@@ -584,6 +832,16 @@ document.getElementById("btnRenameDoc").addEventListener("click", (e)=>{
 
 document.getElementById("btnRemoveDoc").addEventListener("click", (e)=>{
     app_documents.removeDoc_Clicked(app_documents.contextSelectedDoc);
+});
+
+document.getElementById("btnPageRenameDoc").addEventListener("click", (e)=>{
+    app_documents.renamePage_Clicked(app_documents.contextSelectedPage);
+    e.stopPropagation();
+    document.querySelector(".pagesMenu").classList.add("hide");
+});
+
+document.getElementById("btnPageRemoveDoc").addEventListener("click", (e)=>{
+    app_documents.removePage_Clicked(app_documents.contextSelectedPage);
 });
 
 // #endregion DOC CONTEXT MENU EVENT HANDLERS
